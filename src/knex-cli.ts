36f754b3cc1428337983bad03ae69d487009c1c8
Heloise <heloiseap@users.noreply.github.com>
2025-06-13 11:52:15 -0300

    24543

--- src/knex-cli.ts
+++ src/knex-cli.ts
@@ -1,3 +1,8 @@
 import { execSync } from 'node:child_process'
 
-execSync('knex migrate:latest', { stdio: 'inherit' })
+try {
+  execSync('npx knex migrate:latest', { stdio: 'inherit' })
+} catch (err) {
+  console.error('Erro ao executar as migrations:', err)
+  process.exit(1)
+}
